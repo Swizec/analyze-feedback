957d8d9e1e1927a5ebc0f4477282f7d1e735b769
Swizec Teller <swizec@swizec.com>
2024-03-03 17:22:02 -0800

    Will high level summary of summaries work?

--- index.ts
+++ index.ts
@@ -25,7 +25,8 @@ async function readAndParseCSV(filePath: string): Promise<Responses> {
 async function analyzeQuestion(
     responses: Responses,
     question: string,
-    explain: string
+    explain: string,
+    stream?: boolean
 ) {
     const answers = responses
         .filter((r) => r[question].length > 0)
@@ -35,74 +36,126 @@ async function analyzeQuestion(
         )
         .join("\n\n");
 
-    const prompt = `We asked our readers "${question}". Here is a list of their answers. Summarize the answers into common themes and explain ${explain}.\n---\n${answers}`;
-
-    // console.log(prompt);
+    const prompt = `We asked our readers "${question}". Here is a list of their answers. Summarize the answers into 5 high level themes and explain ${explain}.\n---\n${answers}`;
 
     const analysis = await ollama.generate({
         model: "llama2",
         prompt,
         system: systemPrompt,
-        stream: true,
+        // @ts-expect-error
+        stream: !!stream,
     });
 
     return analysis;
 }
 
-async function whatHesitation(responses: Responses) {
+async function whatHesitation(responses: Responses, stream?: boolean) {
     return analyzeQuestion(
         responses,
         "What hesitation did you have about subscribing?",
-        "what hesitations readers have before subscribing"
+        "what hesitations readers have before subscribing",
+        stream
     );
 }
 
-async function whatLearned(responses: Responses) {
+async function whatLearned(responses: Responses, stream?: boolean) {
     return analyzeQuestion(
         responses,
         "What have you learned from Swizec’s Newsletter?",
-        "what readers learned from the newsletter"
+        "what readers learned from the newsletter",
+        stream
     );
 }
 
-async function whatLiked(responses: Responses) {
+async function whatLiked(responses: Responses, stream?: boolean) {
     return analyzeQuestion(
         responses,
         "What have you liked most about the Swizec’s Newsletter?",
-        "what readers liked about the newsletter"
+        "what readers liked about the newsletter",
+        stream
     );
 }
 
-async function whatBenefits(responses: Responses) {
+async function whatBenefits(responses: Responses, stream?: boolean) {
     return analyzeQuestion(
         responses,
         "What are some other benefits you got from Swizec’s Newsletter?",
-        "what benefits readers got from the newsletter"
+        "what benefits readers got from the newsletter",
+        stream
     );
 }
 
-async function whyRecommend(responses: Responses) {
+async function whyRecommend(responses: Responses, stream?: boolean) {
     return analyzeQuestion(
         responses,
         "Would you recommend Swizec’s Newsletter to a friend or coworker? Why?",
-        "why readers would or wouldn't recommend the newsletter"
+        "why readers would or wouldn't recommend the newsletter",
+        stream
     );
 }
 
-async function whatOtherThoughts(responses: Responses) {
+async function whatOtherThoughts(responses: Responses, stream?: boolean) {
     return analyzeQuestion(
         responses,
         "Any other thoughts you'd like to share about Swizec’s Newsletter?",
-        "how readers suggest we can improve"
+        "how readers suggest we can improve",
+        stream
     );
 }
 
-async function whatFor(responses: Responses) {
+async function whatFor(responses: Responses, stream?: boolean) {
     return analyzeQuestion(
         responses,
         "What are you going to use this knowledge for?",
-        "how readers intend to leverage the newsletter"
+        "how readers intend to leverage the newsletter",
+        stream
     );
+}
+
+async function highLevelSummary(responses: Responses) {
+    const questions = [
+        {
+            q: "What hesitation did you have about subscribing?",
+            f: whatHesitation,
+        },
+        {
+            q: "What have you learned from Swizec’s Newsletter?",
+            f: whatLearned,
+        },
+        {
+            q: "What have you liked most about the Swizec’s Newsletter?",
+            f: whatLiked,
+        },
+        {
+            q: "What are some other benefits you got from Swizec’s Newsletter?",
+            f: whatBenefits,
+        },
+        {
+            q: "Would you recommend Swizec’s Newsletter to a friend or coworker? Why?",
+            f: whyRecommend,
+        },
+        {
+            q: "Any other thoughts you'd like to share about Swizec’s Newsletter?",
+            f: whatOtherThoughts,
+        },
+        { q: "What are you going to use this knowledge for?", f: whatFor },
+    ];
+
+    let prompt =
+        "We asked our readers a series of questions. Below is a high level summary of responses to each question. Write a short report on what readers think.";
+
+    for (const { q, f } of questions) {
+        const summary = await f(responses);
+        console.log(`\n\n## ${q}\n\n${summary}`);
+        prompt += `\n\n## ${q}\n\n${summary}`;
+    }
+
+    return ollama.generate({
+        model: "llama2",
+        prompt,
+        system: systemPrompt,
+        stream: true,
+    });
 }
 
 const { positionals } = parseArgs({
@@ -115,11 +168,12 @@ if (!filePath.endsWith(".csv")) {
     throw new Error("You must provide the path to a CSV file");
 }
 
-console.log(`Analyzing ${filePath}`);
+console.log(`Analyzing ${filePath}. This might take a while`);
 
 const data = await readAndParseCSV(filePath);
 
-const analysis = await whatBenefits(data);
+const analysis = await highLevelSummary(data);
+
 for await (const part of analysis) {
     process.stdout.write(part.response);
 }
