d3acf11d0e62395e6c927306060ff386e9441a48
Swizec Teller <swizec@swizec.com>
2024-03-17 18:46:02 -0700

    Sorting by length works pretty good

--- lib/testimonials.ts
+++ lib/testimonials.ts
@@ -14,33 +14,40 @@ const questions = [
     "What are you going to use this knowledge for?",
 ];
 
+function concatResponse(response: Record<string, string>) {
+    const answers = questions.map((q) => ({ q, a: response[q] }));
+    return answers
+        .map(({ q, a }) => `Question: ${q}\nAnswer: ${a}`)
+        .join("\n\n");
+}
+
 export async function writeTestimonial(
     responses: Responses,
     answererIsA: "subscriber" | "reader" | "user"
 ) {
     const topResponses = responses
         .filter((r) => r["Are you enjoying Swizec’s Newsletter?"] === "5")
-        .sort(
-            (a, b) =>
-                new Date(b["Submit Date (UTC)"]).getTime() -
-                new Date(a["Submit Date (UTC)"]).getTime()
-        );
+        .sort((a, b) => concatResponse(b).length - concatResponse(a).length);
+    // .sort(
+    //     (a, b) =>
+    //         new Date(b["Submit Date (UTC)"]).getTime() -
+    //         new Date(a["Submit Date (UTC)"]).getTime()
+    // );
 
     console.log(topResponses.length);
 
     const response = topResponses[0];
-    const answers = questions.map((q) => ({ q, a: response[q] }));
 
     const prompt = `We asked a ${answererIsA} ${
         questions.length
-    } questions about their experience. Turn these answers into a short testimonial. Keep the original phrasing. The testimonial should be short and to the point. It should start with a hesitation then highlight benefits that the ${answererIsA} experienced. Finish with why the ${answererIsA} would suggest this to others. Write a draft then make it shorter. Keep phrasing from the original answers.\n\nHere are the answers:\n\n${answers
-        .map(({ q, a }) => `Question: ${q}\nAnswer: ${a}`)
-        .join("\n\n")}`;
+    } questions about their experience. Turn these answers into a very short testimonial. Keep the original phrasing. The testimonial should be very short and to the point. It should start with a specific hesitation then highlight benefits that the ${answererIsA} experienced. Finish with why the ${answererIsA} would suggest this to others. Keep phrasing from the original answers. Write a draft then make it shorter.\n\nHere are the answers:\n\n${concatResponse(
+        response
+    )}`;
 
     console.log(prompt, "\n\n");
 
     return ollama.generate({
-        model: "mistral",
+        model: "llama2",
         prompt,
         system: systemPrompt,
         stream: true,
